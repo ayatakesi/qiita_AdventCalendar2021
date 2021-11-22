172ea74b61f467297944a4a8475d56e91b7a2f22
ayatakesi <ayanokoji.takesi@gmail.com>
2021-11-23 00:43:23 +0900

    initial commit on local.

---
+++ scripts/misc/docstyle.texi.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+# This script requires libintl-perl(>=1.09).
+use Locale::TextDomain ("docstyle.texi" => "./po/gettext/misc");
+my ($en, $ja);
+while (<>) {
+	print;
+}
---
+++ scripts/misc/emacsver.texi.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+# This script requires libintl-perl(>=1.09).
+use Locale::TextDomain ("emacsver.texi" => "./po/gettext/misc");
+my ($en, $ja);
+while (<>) {
+	print;
+}
